d8bb2ea7b5caaf42fc1bd84a68cedbe2db54e0ec
ksj040506 <ksj040506@naver.com>
2024-12-31 13:31:04 +0900

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private String name;
+    private int balance;
+
+    Account() {}
+
+    public void setName(String name) {
+        this.name = name ;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name ;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
+}
